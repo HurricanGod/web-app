de55742f4513cbb309968a85a6e0f87b136cc7b9
HurricanGod <1807363937@qq.com>
2018-03-27 16:37:13 +0800

    添加@Around的AOP

--- WebApp/src/main/java/cn/hurrican/controller/EmailController.java
+++ WebApp/src/main/java/cn/hurrican/controller/EmailController.java
@@ -8,7 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/email/service/")
@@ -18,7 +22,7 @@ public class EmailController extends BaseController {
     private EmailService emailService;
 
 
-    @SystemLog(level = 1)
+//    @SystemLog(level = 1)
     @RequestMapping(value = "send/{target}/{title}/end.do", produces = "application/json;charset=UTF-8")
     @ResponseBody
     public ResMessage sendEmail(@PathVariable("target") String target, @PathVariable("title") String title) {
@@ -26,5 +30,14 @@ public class EmailController extends BaseController {
         System.out.println("target = " + target);
         System.out.println("title = " + title);
         return ResMessage.creator().logIs("success execute!");
+    }
+
+    @SystemLog(level = 1)
+    @RequestMapping(value = "getMap.do", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public ResMessage sendMap(@RequestParam(required = false) Map<String, Object> model, Integer id){
+        model = model != null ? model : new HashMap<>();
+
+        return ResMessage.creator().logIs("test").putAll(model);
     }
 }
--- common/src/main/java/cn/hurrican/impl/LogServiceProducer.java
+++ common/src/main/java/cn/hurrican/impl/LogServiceProducer.java
@@ -3,17 +3,19 @@ package cn.hurrican.impl;
 import cn.hurrican.model.ResMessage;
 import cn.hurrican.redis.RedisExecutor;
 import cn.hurrican.service.LogService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Aspect
 @Service
 public class LogServiceProducer {
+
+    private static Logger logger = LogManager.getLogger(LogServiceProducer.class);
 
     @Autowired
     private RedisExecutor executor;
@@ -26,6 +28,23 @@ public class LogServiceProducer {
     public void doBefore() {
     }
 
+    @Pointcut("@annotation(cn.hurrican.anotations.SystemLog)")
+    public void doAround(){}
+
+    @Around(value = "doAround()")
+    public ResMessage enhanceTarget(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args = " + args[i]);
+        }
+//        if(args.length == 2){
+//            return ResMessage.creator().logIs("校验失败！");
+//        }
+        Object res = joinPoint.proceed();
+        return (ResMessage) res;
+    }
+
+
     @Before(value = "doBefore()")
     public void checkValidate(JoinPoint joinPoint) {
         System.out.println("execute do before!");
@@ -33,19 +52,20 @@ public class LogServiceProducer {
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
-        throw new RuntimeException("参数非法！");
     }
 
     @AfterReturning(pointcut = "controllerAspect()", returning = "returnVal")
     public ResMessage produceLog(JoinPoint joinPoint, Object returnVal) {
-        executor.doInRedis((instance -> {
-            if (returnVal instanceof ResMessage) {
-                String log = ((ResMessage) returnVal).getLog();
-                Long count = instance.lpush("log", log);
-                System.out.println("count = " + count);
-
-            }
-        }));
+//        executor.doInRedis((instance -> {
+//            if (returnVal instanceof ResMessage) {
+//                String log = ((ResMessage) returnVal).getLog();
+//                Long count = instance.lpush("log", log);
+//                System.out.println("count = " + count);
+//
+//            }
+//        }));
+        logger.info("exec produceLog");
+        System.out.println("exec produceLog");
         return (ResMessage) returnVal;
     }
 }
